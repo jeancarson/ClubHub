2c4a7d5f79a540a200d97a9de1fe697950017187
Darragh Luby <23359048@studentmail.ul.ie>
2024-01-30 03:28:52 +0000

    Add session functionality

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
+# Third party libraries
+from werkzeug import Response
 from flask import (
     Flask,
     render_template,
-    request
+    request,
+    session
 )
 
+# Local modules
 from modules.user_auth import password_match
 
+
 app: Flask = Flask(__name__)
+app.config.from_prefixed_env()  # Load environment variables (from .env and .flaskenv)
+
+
+# Temporary user dictionary with usernames/passwords for login system.
 users: dict[str, str] = {
-    "admin": "243262243132246f6b3835716e6a55446e50304c51675833624962347561566a53646855676179725a61777937706f726d4b73466d6f715975687a75"
+    "admin": "243262243132246f6b3835716e6a55446e50304c51675833624962347561"
+             "566a53646855676179725a61777937706f726d4b73466d6f715975687a75"
 }
 
 
@@ -16,30 +26,60 @@ users: dict[str, str] = {
 @app.route("/index")
 @app.route("/home")
 def home() -> str:
+    """
+    Loads the home (default) page.
+    """
+
+    if "user" in session:
+        user: str = session["user"]
+        return render_template("index.html", header=f"Hello {user}!")
+
     return render_template("index.html")
 
 
 @app.route("/login")
-def login_home() -> str:
+def login() -> str:
+    """
+    Loads the login page.
+    """
+
     return render_template("login.html")
 
 
 @app.route("/login", methods=["POST"])
-def login() -> str:
+def login_post() -> str | Response:
+    """
+    Function called when login button (from the login page) is pressed.
+    """
+
     username: str = request.form["username"]
     password: str = request.form["password"]
-    hashed_pw: str | None = users.get(username)
 
-    if hashed_pw is None:
-        app.logger.warning(f"Login fail: {username!r} does not exist")
-        return render_template("login.html")
+    if "user" in session:
+        current_user: str = session["user"]
+        app.logger.warning(f"Login fail: {current_user!r} is already logged in")
+        return render_template("login.html", error_message=f"You are already logged in as {current_user!r}")
+
+    if not username:
+        app.logger.warning("Login fail: Empty username")
+        return render_template("login.html", error_message="Please enter a username")
+
+    if username not in users:
+        app.logger.warning(f"Login fail: User {username!r} does not exist")
+        return render_template("login.html", error_message="User not found")
+
+    hashed_pw: str = users[username]
 
     if not password_match(password, hashed_pw):
-        app.logger.info(f"Login fail: Incorrect password for user {username!r}")
-        return render_template("login.html")
+        app.logger.warning(f"Login fail: Incorrect password for user {username!r}")
+        return render_template("login.html", error_message="Incorrect password")
 
     app.logger.info(f"Login success: {username!r}")
-    return render_template("index.html")
+
+    # Create a user session
+    session["user"] = username
+
+    return render_template("index.html", header=f"Hello {username}!")
 
 
 if __name__ == '__main__':
