7f4d63a6d4491a72f36075fb36eb3c22b5f9734a
jeancarson <jeanloucarson@gmail.com>
2024-02-13 17:04:53 +0000

    lets make some branches

--- application/blueprints/jean_blueprint.py
+++ application/blueprints/jean_blueprint.py
@@ -7,32 +7,50 @@ jean_blueprint = Blueprint("jean_blueprint", __name__)
 club_id = 6
 coordinator_name = "John Doe"
 
-@jean_blueprint.route("/cohome", methods=["GET"])
+
+
+@jean_blueprint.route("/cohome")
 def cohome():
     if "user" in session:
         user: str = session["user"]
         return render_template("html/index.html", header=f"Hello {user}!")
+    club_id = club_id
+    club_details = dbf.query_db(ecd.get_club_details.format(club_id=club_id))
+    club_name = club_details["club_name"]
+    club_description = club_details["club_description"]
     number_of_active_users = dbf.query_db(ecd.count_approved_members.format(club_id=club_id))
     number_of_pending_users = dbf.query_db(ecd.count_pending_members.format(club_id=club_id))
-    return render_template("html/Coordinator/coordinator_dashboard.html", coordinator_name=coordinator_name, club_id = club_id, active_users=number_of_active_users, pending_users=number_of_pending_users, club_title = "Club 1", club_description = "Description 1")
+    #TODO add the for loop thingy to display details of each event.
+    #TODO this is kinda fine for dispaying info I hope... but need to add the functionality to write to the database when save is pressed.
+    return render_template("html/Coordinator/coordinator_dashboard.html", coordinator_name=coordinator_name, club_id = club_id, active_users=number_of_active_users, pending_users=number_of_pending_users, club_title = club_name, club_description = club_description)
 
-  
-  def display_event_details():
+
+@jean_blueprint.route("/cohome", methods=["POST"])
+def save_club_details():
+    club_name = request.form["club_name"]
+    club_description = request.form["club_description"]
+    number_of_active_users = dbf.query_db(ecd.count_approved_members.format(club_id=club_id))
+    number_of_pending_users = dbf.query_db(ecd.count_pending_members.format(club_id=club_id))
+    dbf.modify_db(ecd.save_club_details.format(club_id=club_id, club_name=club_name, club_description=club_description))
+    return render_template("html/Coordinator/coordinator_dashboard.html", coordinator_name=coordinator_name, club_id = club_id, active_users=number_of_active_users, pending_users=number_of_pending_users, club_title = club_name, club_description = club_description)
+
+
+
+
+
+
+def display_event_details():
     limited_upcoming_events = dbf.query_db(ecd.limited_view_all_upcoming_events.format(club_id=club_id))
     for event in limited_upcoming_events:
         number_of_approved_participants = dbf.query_db(ecd.count_approved_participants.format(event_id=event["event_id"]))
         number_of_pending_participants = dbf.query_db(ecd.count_pending_participants.format(event_id=event["event_id"]))
         return render_template("html/Coordinator/coordinator_dashboard.html", coordinator_name=coordinator_name, club_id = club_id, club_title = "Club 1", club_description = "Description 1", upcoming_events=limited_upcoming_events, approved_participants=number_of_approved_participants, pending_participants=number_of_pending_participants)
 
-    return render_template("html/Coordinator/coordinator_dashboard.html", coordinator_name="John Doe", active_users=40,
-                           pending_users=3, club_title="Club 1", club_description="Description 1")
-
-
 @jean_blueprint.route("/menview/<status>")
 def parview(status):
     if "user" in session:
         user: str = session["user"]
-        return render_template("html/default-home.html", header=f"Hello {user}!")
+        return render_template("html/index.html", header=f"Hello {user}!")
 
     return render_template("html/Coordinator/view_participants.html", status=status)
 
@@ -41,7 +59,7 @@ def parview(status):
 def memview(status):
     if "user" in session:
         user: str = session["user"]
-        return render_template("html/default-home.html", header=f"Hello {user}!")
+        return render_template("html/index.html", header=f"Hello {user}!")
 
     return render_template("html/Coordinator/member_view.html", status=status)
 
@@ -50,7 +68,7 @@ def memview(status):
 def see_events(timeline):
     if "user" in session:
         user: str = session["user"]
-        return render_template("html/default-home.html", header=f"Hello {user}!")
+        return render_template("html/index.html", header=f"Hello {user}!")
 
     return render_template("html/Coordinator/multi_event_view.html", timeline=timeline)
 
@@ -59,7 +77,7 @@ def see_events(timeline):
 def edit_event():
     if "user" in session:
         user: str = session["user"]
-        return render_template("html/default-home.html", header=f"Hello {user}!")
+        return render_template("html/index.html", header=f"Hello {user}!")
 
     return render_template("html/Coordinator/single_event_view.html", event_name='Event 1', event_date='2021-10-10',
                            event_time='10:00', event_location='Location 1', event_description='Description 1')
